CollaboraOnline/online

Send "authenticated" as a JSON message
...as the code in global.collabFetchFile (browser/js/global.js) expects to
receive it as a JSON message

Signed-off-by: Stephan Bergmann <stephan.bergmann@collabora.com>
Change-Id: I3a2f0f41b9a5e104d115fdfa598dee4d3f2802e0

wsd/CollabSocketHandler.cpp
@@ -167,7 +167,7 @@ void CollabSocketHandler::onCheckFileInfoFinished(CheckFileInfo& cfi)
                     << ", userId: " << COOLWSD::anonymizeUsername(_userId)
                     << ", username: " << COOLWSD::anonymizeUsername(_username)
                     << ", canWrite: " << _userCanWrite);
-            sendMessage("authenticated");
+            sendMessage("{\"type\":\"authenticated\"}");
             break;
         }
         case CheckFileInfo::State::Unauthorized:

stbergmann/core

wsd/CollabSocketHandler.cpp
@@ -167,7 +167,7 @@ void CollabSocketHandler::onCheckFileInfoFinished(CheckFileInfo& cfi)
                     << ", userId: " << COOLWSD::anonymizeUsername(_userId)
                     << ", username: " << COOLWSD::anonymizeUsername(_username)
                     << ", canWrite: " << _userCanWrite);
-            sendTextMessage("authenticated");
+            sendTextMessage("{\"type\":\"authenticated\"}");
             break;
         }
         case CheckFileInfo::State::Unauthorized: